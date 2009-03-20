695f1f9700fb9e07e93cfe48a62d11af16ac3dd4
Carl-Johan Sveningsson <cj@yoga.ee>
2009-03-20 17:09:09 +0200

    Initial commit

---
+++ Octave/plotnormaldist.m
@@ -0,0 +1,14 @@
+% range min
+% range max
+% number per bucket
+% number of buckets
+
+% resultMean
+% resultVariance
+% resultRangeMin
+% resultRangeMax
+
+rangeMinP = 0;
+rangeMaxP = 10;
+numbersPerBucketP = 1;
+numberOfBucketsP = 10;
